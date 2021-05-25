6571bd9b1b0c1be8f3dc4154e78fbbdf2cfdb446
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2021-05-25 14:05:30 +0900

    Feat insert record

--- cmd/insert/main.go
+++ cmd/insert/main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"cloud.google.com/go/spanner"
+)
+
 func main() {
+	ctx := context.Background()
+	spannerClient, err := spanner.NewClient(ctx, "projects/local-project/instances/test-instance/databases/local-database")
+	if err != nil {
+		panic(err)
+	}
+
+	err = insertUsers(ctx, spannerClient)
+	if err != nil {
+		panic(err)
+	}
+
 	return
+}
+
+func insertUsers(ctx context.Context, client *spanner.Client) error {
+	rand.Seed(time.Now().UnixNano())
+	min := 1000000000
+	max := 9000000000
+	_, err := client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		stmts := []spanner.Statement{}
+		for i := 1; i <= 100; i++ {
+			r := rand.Intn(max-min) + min
+			userID := fmt.Sprintf("User %d", r)
+			stmts = append(stmts, spanner.Statement{
+				SQL: "INSERT INTO UserScore (UserID, Score, UpdatedAt) VALUES (@userID, @score, @updatedAt)",
+				Params: map[string]interface{}{
+					"userID":    userID,
+					"score":     r,
+					"updatedAt": time.Date(2020, 3, 10, 23, 59, 59, 999, time.UTC),
+				},
+			})
+		}
+		_, err := txn.BatchUpdate(ctx, stmts)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/knocknote/spanner_test
 
 go 1.16
+
+require (
+	cloud.google.com/go/spanner v1.18.0
+	google.golang.org/grpc v1.38.0
+)
